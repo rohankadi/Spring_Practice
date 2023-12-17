34fc1f68262f94efec1e36b5832d9a8d13a64aa0
Rohan Kadi <jeevarhn.rk174@gmail.com>
2023-12-17 22:26:54 +0530

    Add files via upload

---
+++ Spring_Project6_AutoWiring_byType/src/main/java/com/kodnest/Spring_Project6_AutoWiring_constructor/App.java
@@ -0,0 +1,14 @@
+package com.kodnest.Spring_Project6_AutoWiring_constructor;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext cxt = new ClassPathXmlApplicationContext("config.xml");
+		Mobile mobile1 = (Mobile) cxt.getBean("mob");
+		System.out.println(mobile1);
+
+		Person p = (Person) cxt.getBean("person1");
+		System.out.println(p);
+	}
+}
